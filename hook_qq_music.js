a51fe58260640a4c61bb23f4f74b3e9e4fff4047
Yang He <conradsheeran2005@gmail.com>
2025-07-01 23:17:47 +0800

    feat: Adapt to the new version of QQ Music

--- hook_qq_music.js
+++ hook_qq_music.js
@@ -1,82 +1,72 @@
 const TARGET_DLL = "QQMusicCommon.dll";
 
-var EncAndDesMediaFileConstructorAddr = Module.findExportByName(
-  TARGET_DLL,
-  "??0EncAndDesMediaFile@@QAE@XZ"
-);
+function findAndLog(dllName, funcName) {
+  const module = Process.getModuleByName(dllName);
+  if (!module) {
+    throw new Error(`Could not find module '${dllName}'. QQ Music might not have loaded it yet or the name is incorrect.`);
+  }
 
-var EncAndDesMediaFileDestructorAddr = Module.findExportByName(
-  TARGET_DLL,
-  "??1EncAndDesMediaFile@@QAE@XZ"
-);
+  const address = module.findExportByName(funcName);
 
-var EncAndDesMediaFileOpenAddr = Module.findExportByName(
-  TARGET_DLL,
-  "?Open@EncAndDesMediaFile@@QAE_NPB_W_N1@Z"
-);
+  console.log(`Searching for ${funcName} in ${dllName}... Address: ${address}`);
+  if (address === null) {
+      throw new Error(`Could not find export '${funcName}' in ${dllName}. QQ Music version might be incompatible.`);
+  }
+  return address;
+}
 
-var EncAndDesMediaFileGetSizeAddr = Module.findExportByName(
-  TARGET_DLL,
-  "?GetSize@EncAndDesMediaFile@@QAEKXZ"
-);
+function initialize() {
+    try {
+        var EncAndDesMediaFileConstructorAddr = findAndLog(TARGET_DLL, "??0EncAndDesMediaFile@@QAE@XZ");
+        var EncAndDesMediaFileDestructorAddr = findAndLog(TARGET_DLL, "??1EncAndDesMediaFile@@QAE@XZ");
+        var EncAndDesMediaFileOpenAddr = findAndLog(TARGET_DLL, "?Open@EncAndDesMediaFile@@QAE_NPB_W_N1@Z");
+        var EncAndDesMediaFileGetSizeAddr = findAndLog(TARGET_DLL, "?GetSize@EncAndDesMediaFile@@QAEKXZ");
+        var EncAndDesMediaFileReadAddr = findAndLog(TARGET_DLL, "?Read@EncAndDesMediaFile@@QAEKPAEK_J@Z");
+        var EncAndDesMediaFileConstructor = new NativeFunction(EncAndDesMediaFileConstructorAddr, "pointer", ["pointer"], "thiscall");
+        var EncAndDesMediaFileDestructor = new NativeFunction(EncAndDesMediaFileDestructorAddr, "void", ["pointer"], "thiscall");
+        var EncAndDesMediaFileOpen = new NativeFunction(EncAndDesMediaFileOpenAddr, "bool", ["pointer", "pointer", "bool", "bool"], "thiscall");
+        var EncAndDesMediaFileGetSize = new NativeFunction(EncAndDesMediaFileGetSizeAddr, "uint32", ["pointer"], "thiscall");
+        var EncAndDesMediaFileRead = new NativeFunction(EncAndDesMediaFileReadAddr, "uint", ["pointer", "pointer", "uint32", "uint64"], "thiscall");
 
-var EncAndDesMediaFileReadAddr = Module.findExportByName(
-  TARGET_DLL,
-  "?Read@EncAndDesMediaFile@@QAEKPAEK_J@Z"
-);
+        rpc.exports = {
+          decrypt: function (srcFileName, tmpFileName) {
+            var EncAndDesMediaFileObject = Memory.alloc(0x28);
+            EncAndDesMediaFileConstructor(EncAndDesMediaFileObject);
 
-var EncAndDesMediaFileConstructor = new NativeFunction(
-  EncAndDesMediaFileConstructorAddr,
-  "pointer",
-  ["pointer"],
-  "thiscall"
-);
+            var fileNameUtf16 = Memory.allocUtf16String(srcFileName);
+            var success = EncAndDesMediaFileOpen(EncAndDesMediaFileObject, fileNameUtf16, 1, 0);
 
-var EncAndDesMediaFileDestructor = new NativeFunction(
-  EncAndDesMediaFileDestructorAddr,
-  "void",
-  ["pointer"],
-  "thiscall"
-);
+            if (!success) {
+                console.error(`Failed to open file: ${srcFileName}`);
+                EncAndDesMediaFileDestructor(EncAndDesMediaFileObject);
+                return;
+            }
 
-var EncAndDesMediaFileOpen = new NativeFunction(
-  EncAndDesMediaFileOpenAddr,
-  "bool",
-  ["pointer", "pointer", "bool", "bool"],
-  "thiscall"
-);
+            var fileSize = EncAndDesMediaFileGetSize(EncAndDesMediaFileObject);
+            console.log(`File size: ${fileSize}`);
 
-var EncAndDesMediaFileGetSize = new NativeFunction(
-  EncAndDesMediaFileGetSizeAddr,
-  "uint32",
-  ["pointer"],
-  "thiscall"
-);
+            if (fileSize === 0) {
+                console.warn(`File size is 0 for: ${srcFileName}`);
+                EncAndDesMediaFileDestructor(EncAndDesMediaFileObject);
+                return;
+            }
 
-var EncAndDesMediaFileRead = new NativeFunction(
-  EncAndDesMediaFileReadAddr,
-  "uint",
-  ["pointer", "pointer", "uint32", "uint64"],
-  "thiscall"
-);
+            var buffer = Memory.alloc(fileSize);
+            EncAndDesMediaFileRead(EncAndDesMediaFileObject, buffer, fileSize, 0);
 
-rpc.exports = {
-  decrypt: function (srcFileName, tmpFileName) {
-    var EncAndDesMediaFileObject = Memory.alloc(0x28);
-    EncAndDesMediaFileConstructor(EncAndDesMediaFileObject);
+            var data = buffer.readByteArray(fileSize);
+            EncAndDesMediaFileDestructor(EncAndDesMediaFileObject);
 
-    var fileNameUtf16 = Memory.allocUtf16String(srcFileName);
-    EncAndDesMediaFileOpen(EncAndDesMediaFileObject, fileNameUtf16, 1, 0);
+            var tmpFile = new File(tmpFileName, "wb");
+            tmpFile.write(data);
+            tmpFile.close();
+            console.log(`Decryption complete. Saved to: ${tmpFileName}`);
+          },
+        };
+    } catch (e) {
+        console.error("An error occurred during agent initialization:");
+        console.error(e.stack);
+    }
+}
 
-    var fileSize = EncAndDesMediaFileGetSize(EncAndDesMediaFileObject);
-
-    var buffer = Memory.alloc(fileSize);
-    EncAndDesMediaFileRead(EncAndDesMediaFileObject, buffer, fileSize, 0);
-
-    var data = buffer.readByteArray(fileSize);
-    EncAndDesMediaFileDestructor(EncAndDesMediaFileObject);
-    
-    var tmpFile = new File(tmpFileName, "wb");
-    tmpFile.write(data);
-  },
-};
+setImmediate(initialize);
